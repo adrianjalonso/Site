6732421c6446c97d0981b37ea973bb5dc69d5551
Adrián Alonso <adrianjalonso@gmail.com>
2025-06-27 14:45:43 -0300

    ok

--- script.js
+++ script.js
@@ -11,7 +11,7 @@
     const minutos = Math.floor((tempoRestante / (1000 * 60)) % 60);
     const segundos = Math.floor((tempoRestante / 1000) % 60);
 
-    document.getElementById("dias").textContent = String(dias).padStart(3, "0");
+    document.getElementById("dias").textContent = String(dias).padStart(2, "0");
     document.getElementById("horas").textContent = String(horas).padStart(2, "0");
     document.getElementById("minutos").textContent = String(minutos).padStart(2, "0");
     document.getElementById("segundos").textContent = String(segundos).padStart(2, "0");
@@ -39,13 +39,13 @@
     }
   } 
 
-  function esconderMenu() {
+  function escondeuMenu() {
     menu.style.display = 'none'
   }
 
   function rolou(){
     let menu = document.getElementById('menu')
-    if (window.innerWidth == 768){
+    if (window.innerWidth <= 768){
       menu.style.display = 'none'
     } else {
       menu.style.display = 'block'
